0bc5b144ec3a18a2f63d17ec789ecb6276bbce43
abdkhaleel <abdkhaleel16@gmail.com>
2025-11-20 12:13:26 +0530

    auth access fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,15 @@
-import { withAuth } from "next-auth/middleware";
+import { withAuth } from 'next-auth/middleware';
 
 export default withAuth({
-    callbacks: {
-        authorized: ({ token }) => {
-            return !!token && token.role === 'admin';
-        },
+  callbacks: {
+    authorized: ({ token }) => {
+      return !!token && token.role === 'admin';
     },
+  },
 });
 
 export const config = {
-    matcher: [
-        '/admin/:path*',
-    ],
+  matcher: [
+    '/admin/:path*', 
+  ],
 };
